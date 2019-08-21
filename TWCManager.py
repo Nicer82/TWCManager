6e20742d2fd66fc8bda1971880f5961e0f54b28a
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-08-21 17:08:10 +0200

    Update TWCManager.py

--- TWCManager.py
+++ TWCManager.py
@@ -245,7 +245,7 @@ emPassword = 'EnergyMonitor'
 # 9 includes raw RS-485 messages transmitted and received (2-3 per sec)
 # 10 is all info.
 # 11 is more than all info.  ;)
-debugLevel = 1
+debugLevel = 10
 
 # Choose whether to display milliseconds after time on each line of debug info.
 displayMilliseconds = False
